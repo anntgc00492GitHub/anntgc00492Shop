284dc346476a3a508426886f0a0f566161a3efa1
anntgc00492GitHub <anntgc00492@gmail.com>
2016-08-11 17:34:59 +0700

    add entity framework code first for data

---
+++ anntgc00492Shop.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anntgc00492Shop.Data.Infrastructure
+{
+    public class UnitOfWork:IUnitOfWork
+    {
+        private readonly IDbFactory dbFactory;
+        private Anntgc00492ShopDbContext dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        public Anntgc00492ShopDbContext DbContext
+        {
+            get { return dbContext ?? (dbContext = dbFactory.Init()); }
+        }
+
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+}
