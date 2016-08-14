2c1fd4b5c022a57b5e68d8e5577e136e91ec03d4
anntgc00492GitHub <anntgc00492@gmail.com>
2016-08-14 16:21:55 +0700

    add lai

---
+++ anntgc00492Shop.Data/Repositories/FooterRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using anntgc00492Shop.Data.Infrastructure;
+using anntgc00492Shop.Model.Models;
+
+namespace anntgc00492Shop.Data.Repositories
+{
+    public interface IFooterRepository : IRepository<Footer>
+    {
+    }
+
+    public class FooterRepository : RepositoryBase<Footer>, IFooterRepository
+    {
+        public FooterRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
