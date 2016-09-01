d5ce7cb87f258b2b8fa1df4ebfabaec5277d94b8
anntgc00492GitHub <anntgc00492@gmail.com>
2016-09-02 05:20:38 +0700

    what is scope

---
+++ anntgc00492Shop.Web/Scripts/spa/app.js
@@ -0,0 +1,21 @@
+﻿var myApp = angular.module('myModule', []);
+
+
+//myApp.controller("myController", myController);
+//myApp.$inject = ["$scope"];
+//myApp.controller("schoolController", schoolController);
+
+
+
+//declare
+myApp.controller("SchoolController", function($scope) {
+    $scope.thongDiep = "thong bao tu truong";
+});
+
+myApp.controller("TeacherController", function ($scope) {
+    $scope.thongDiep = "Thong diep den giao vien";
+});
+
+myApp.controller("StudentController", function ($scope) {
+    //$scope.thongDiep = "thong diep student Controller";
+});
