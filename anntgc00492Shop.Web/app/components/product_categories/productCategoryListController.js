c4cca6ce4a4162e918176d77f42e46e895ffeb21
anntgc00492GitHub <anntgc00492@gmail.com>
2016-09-06 14:17:14 +0700

    Trien khai phan trang

--- anntgc00492Shop.Web/app/components/product_categories/productCategoryListController.js
+++ anntgc00492Shop.Web/app/components/product_categories/productCategoryListController.js
@@ -1,16 +1,31 @@
 ﻿(function (app) {
     app.controller('productCategoryListController', productCategoryListController);
+
     productCategoryListController.$inject = ['$scope', 'apiService'];
+
     function productCategoryListController($scope, apiService) {
         $scope.productCategories = [];
+        $scope.page = 0;
+        $scope.pagesCount = 0;
         $scope.getProductCategories = getProductCategories;
         $scope.getProductCategories();
-        function getProductCategories() {
-            apiService.get('/api/productCategory/getallparents', null, function (result) {
-                $scope.productCategories = result.data;
-            }, function () { 
-                console.log('load product categories failed');
+        function getProductCategories(page) {
+            page = page || 0;
+            var config = {
+                params: {
+                    page: page,
+                    pageSize: 2
+                }
+            }
+            apiService.get('/api/productcategory/getall', config, function (result) {
+                $scope.productCategories = result.data.Items;
+                $scope.page = result.data.Page;
+                $scope.pagesCount = result.data.TotalPages;
+                $scope.totalCount = result.data.TotalCount;
+            }, function () {
+                console.log('Load productcategory failed.');
             });
         }
+
     }
-})(angular.module('anntgc00492Shop.products'));
+})(angular.module('anntgc00492Shop.product_categories'));
