85b0e1afd0738221a5650ce8e9f12dda4ec58feb
anntgc00492GitHub <anntgc00492@gmail.com>
2016-09-18 16:47:35 +0700

    Ghep form dang nhap xong, chuyen app.js trong asset vao baseview

--- anntgc00492Shop.Web/app/app.js
+++ anntgc00492Shop.Web/app/app.js
@@ -7,19 +7,21 @@
     config.$inject = ['$stateProvider', '$urlRouterProvider'];
     function config($stateProvider, $urlRouterProvider) {
         $stateProvider
-            .state('base',
-            {
+            .state('base', {
                 url: '',
-                templateUrl: "/app/shared/views/baseView.html",
+                templateUrl: '/app/shared/views/baseView.html',
                 abstract: true
+            }).state('login', {
+                url: "/login",
+                templateUrl: "/app/components/login/loginView.html",
+                controller: "loginController"
             })
             .state('home', {
                 url: "/admin",
-                parent:'base',
+                parent: 'base',
                 templateUrl: "/app/components/home/homeView.html",
                 controller: "homeController"
             });
-            
-        $urlRouterProvider.otherwise("/admin");
+        $urlRouterProvider.otherwise('/login');
     }
 })();
--- anntgc00492Shop.Web/app/components/login/loginController.js
+++ anntgc00492Shop.Web/app/components/login/loginController.js
@@ -1,1 +1,9 @@
-﻿
+﻿(function(app) {
+    app.controller('loginController', loginController);
+    loginController.$inject = ['$scope', '$state'];
+    function loginController($scope, $state) {
+        $scope.loginSubmit = function() {
+            $state.go('home');
+        }
+    }
+})(angular.module('anntgc00492Shop'));
